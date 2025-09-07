9ed1ce1a324ae1df2bf74887a7f1ecbe7e510be5
kishore152 <33343140+kishore152@users.noreply.github.com>
2025-09-07 07:22:21 +0000

    Deploying to gh-pages from @ kishore152/dualproject@844351bdb1f08ff679a2efb7c3cf32a55ed0c1da 🚀

---
+++ chunk-MS2DZ7QG.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-IQXM3YOF.js";import"./chunk-FPOI45JT.js";import"./chunk-PWHCFNGU.js";import"./chunk-EQDQRRRY.js";export{a as PrivacypolicyComponent};
---
+++ login/browser/assets/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  API_BASE_URL: 'http://127.0.0.1:5000/app/api',
+  assetBasePathLogin : '', 
+  assetBasePathHome : '', 
+  isDevEnvironment : true
+};
+
